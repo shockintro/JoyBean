265583d360f715746bcfcf190dc98fee6d22c86d
Administrator <Administrator@LS--20160714IDP>
2017-01-06 13:11:23 +0800

    test

---
+++ src/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=1;
+		for (int i = 0; i < 64; i++) {
+			System.out.println(sum++);
+		}
+	}
+
+}
